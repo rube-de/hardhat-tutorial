fb32c767a69eaba4410d665aa5de8c93cb632c82
rube-de <rube@ruf2.de>
2021-04-26 00:34:11 +0200

    deploy to ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and write the alechemy key 
+// and the ropsten private key in a secret.json
+const { ALCHEMY_API_KEY, ROPSTEN_PRIVATE_KEY } = require('./.secrets.json');
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
